b2a627d9085935019475a494d6770223af8f7129
seascabs <seascabs@gmail.com>
2020-07-10 19:55:31 -0500

    Insane movement
    jiggles.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,23 +2,44 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 
+#distorted view, phantom stuff, involuntary movement, cant hold onto vines
+var insanityLevel = 0
+var insaneMoveCount = 0
+const baseInsanityMovement = 2
+var isInsane = false
+
 var velocity = Vector2()
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	SetTimer()
+	pass
+	
+func SetTimer():
+	timer = Timer.new()
+	timer.set_wait_time( 2 )
+	timer.connect("timeout", self, "timeOut")
+	add_child(timer) #to process
+	timer.start()
+	
+func timeOut():
+	timer.set_wait_time(randf()*5.0+1.0)
+	timer.start()
+	
+	#test code
+	if(insanityLevel < 6):
+		insanityLevel += 1
+		
+	isInsane = true
 
 func get_input():
+	if (isInsane and insanityLevel > 0):
+		HandleInsaneMovement()
+	else:
+		HandleNormalMovement()
+
+func HandleNormalMovement():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
@@ -28,12 +49,23 @@ func get_input():
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
+	
 	velocity = velocity.normalized() * speed
+
+func HandleInsaneMovement():
+	velocity.x = randf()*2.0-1.0
+	velocity.y = randf()*2.0-1.0
+	velocity = velocity.normalized() * (speed) * (insanityLevel)
+	
+	insaneMoveCount+=1
+	if(insaneMoveCount >= 30):
+		isInsane = false
+		insaneMoveCount = 0
+		
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-	
 	
 func _on_body_entered(_body):
 	velocity = 1
