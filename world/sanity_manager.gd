e266d9db6f6e0bddb92f9ca079a109781d28910a
jasonmccollum <jasonmccollum1@gmail.com>
2020-07-11 12:44:58 -0400

    Removing input event

--- world/sanity_manager.gd
+++ world/sanity_manager.gd
@@ -6,12 +6,6 @@ export var max_insanity = 100
 
 var current_insanity = 0
 
-func _input(event):
-	if event.is_action_pressed("up"):
-		change_insanity(10)
-	elif event.is_action_pressed("down"):
-		change_insanity(-10)
-
 func change_insanity(delta: float):
 	current_insanity += delta
 	current_insanity = clamp(current_insanity, 0, max_insanity)
