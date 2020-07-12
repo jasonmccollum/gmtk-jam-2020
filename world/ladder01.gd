9d70a4deddf2ac35355d1349164fcdc821e8d61d
jasonmccollum <jasonmccollum1@gmail.com>
2020-07-11 20:42:14 -0400

    Adding base game

--- world/ladder01.gd
+++ world/ladder01.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 func _on_Area2D_body_entered(body):
-	if(body.name == "Player"):
+	if(body.name == "player"):
 		body.canClimb = true
 
 func _on_Area2D_body_exited(body):
-	if(body.name == "Player"):
+	if(body.name == "player"):
 		body.canClimb = false
