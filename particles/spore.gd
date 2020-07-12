1d6012269857561391a1d132e687e66eaeb86185
jasonmccollum <jasonmccollum1@gmail.com>
2020-07-12 13:39:45 -0400

    Fixing flashlight kill bug

--- particles/spore.gd
+++ particles/spore.gd
@@ -5,7 +5,13 @@ export var max_y = 1600
 
 var death_scene = preload("res://particles/sporedeath.tscn")
 
+var flashlight_ref = null
+
 func _physics_process(delta):
+	if flashlight_ref:
+		if flashlight_ref.visible:
+			die()
+
 	if!("tip" in get_parent().name):
 		position.y -= move_speed * delta
 	
@@ -29,5 +35,11 @@ func _on_Area2D_body_entered(body):
 		die()
 
 func _on_Area2D_area_entered(area):
+	var is_on_flashlight = area.get_parent().name == 'flashlight' and area.get_parent().visible
+	var is_torch = area.get_parent().name == 'flashlight' and area.get_parent().get("activated")
+	
 	if area.get_parent().name == 'flashlight':
+		flashlight_ref = area.get_parent()
+
+	if is_on_flashlight or is_torch:
 		die()
