211ad4be2972b448f73e5cfbbac6692a5ad65b6a
seascabs <seascabs@gmail.com>
2020-07-12 08:42:15 -0500

    spores insane torch sane

--- Player.gd
+++ Player.gd
@@ -155,6 +155,8 @@ func _physics_process(delta):
 	if is_on_floor(): on_floor = true
 	else: on_floor = false
 	
+	position.x = clamp(position.x, 0, 1900)
+	
 	
 func _on_body_entered(_body):
 	velocity = 1
--- world/sanity_manager.gd
+++ world/sanity_manager.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
+onready var sanityBar = $SanityBar
+
 signal update_insanity(new_insanity)
 
 export var max_insanity = 100
 
 var current_insanity = 0
 
+func _ready():
+	connect("timeout", self, "_on_Timer_timeout")
+	
+#func _process(delta):
+	#change_insanity(delta)
+
 func change_insanity(delta: float):
 	current_insanity += delta
 	current_insanity = clamp(current_insanity, 0, max_insanity)
+	sanityBar.setSanity(current_insanity)
+	
 	emit_signal("update_insanity", current_insanity)
-	print(current_insanity)
